a1432bd973f181197ccbfb7cb17f8057714fc6b5
Toomas Vahtra <toomas.vahtra@gmail.com>
2016-09-30 17:58:06 +0300

    added help module

---
+++ modules/get_help.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+module.exports = 
+{
+	/*
+	 * Get help for modules 
+	 **/
+	get_help: function(text, cb) {
+		var command = text.split(" ")[1];
+		if ( command !== undefined ) {
+			var dyna = JSON.parse(fs.readFileSync('./modules.json', 'utf8'));
+                	dyna.modules.forEach(function(value, key) {
+                		var re = new RegExp(command, 'i');
+				if ( re.test(value.command) ) {
+					cb(value.description);
+					return;
+				}
+			});
+		}
+		else {
+			cb('Usage: !help <command>');
+		}	
+	}
+	
+}
