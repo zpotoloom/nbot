21b01ecbc5fc1604a31097a19a2411041321d88b
Toomas Vahtra <toomas.vahtra@gmail.com>
2016-10-04 09:59:55 +0300

    get_boobs

---
+++ modules/get_boobs.js
@@ -0,0 +1,18 @@
+var gis = require('g-i-s');
+
+module.exports = {
+
+	get_boobs: function(text, cb) {
+		gis('boobs', logResults);
+		
+		function logResults(error, results) {
+		  if (error) {
+		    console.log(error);
+		  }
+		  else {
+		    cb(results[Math.floor( Math.random() * ( results.length - 1 ) )].url);
+		    //console.log(JSON.stringify(results, null, '  '));
+		  }
+		}
+	}
+}
