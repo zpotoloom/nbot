461014b73caef38ae576cfd94fed51d991137544
zpoto <toomas.vahtra@gmail.com>
2016-10-29 19:08:17 +0300

    Update get_pic.js

--- modules/get_pic.js
+++ modules/get_pic.js
@@ -16,7 +16,7 @@ module.exports = {
 		  }
 		  else {
 		    var result = results[Math.floor( Math.random() * ( results.length - 1 ) )];
-		    if ( result.url !=== undefined ) {
+		    if ( result.url !== undefined ) {
 		    	cb(result.url);
 		    } else {
 			cb('Try harder!');
