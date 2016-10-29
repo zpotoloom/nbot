b84db179a146c84bb771431e0a725f21f1470825
Toomas Vahtra <toomas.vahtra@gmail.com>
2016-10-29 15:56:09 +0300

    allow multiple searchterms in get_pic

--- modules/get_pic.js
+++ modules/get_pic.js
@@ -3,8 +3,9 @@ var gis = require('g-i-s');
 module.exports = {
 
 	get_pic: function(from, text, cb) {
-		var query = text.split(' ');
-		gis(query[1] + ' site:tumblr.com', logResults);
+		var query = text.replace(/[^ ]* /, '');
+		
+		gis(query + ' site:tumblr.com', logResults);
 		
 		function logResults(error, results) {
 		  if (error) {
