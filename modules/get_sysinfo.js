a84c07097b627122d79651bb64f34adcc74f3efa
Toomas Vahtra <toomas.vahtra@gmail.com>
2016-12-31 20:08:57 +0200

    added sysinfo plugin

---
+++ modules/get_sysinfo.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+	get_sysinfo: function(from, text, cb) {
+    var unit = ['', 'K', 'M', 'G', 'T', 'P'];
+    
+    function bytesToSize(input, precision)
+    {
+        var index = Math.floor(Math.log(input) / Math.log(1024));
+        if (unit >= unit.length) return input + ' B';
+        return (input / Math.pow(1024, index)).toFixed(precision) + ' ' + unit[index] + 'B'
+    }
+    
+    function format(seconds){
+      function pad(s){
+        return (s < 10 ? '0' : '') + s;
+      }
+      var hours = Math.floor(seconds / (60*60));
+      var minutes = Math.floor(seconds % (60*60) / 60);
+      var seconds = Math.floor(seconds % 60);
+    
+      return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+    } 
+    
+    usage = process.memoryUsage();
+    cb(  'Uptime: ' + format(process.uptime()) +
+         ' { RSS: ' + bytesToSize(usage.rss, 3),
+         'and Heap:', bytesToSize(usage.heapUsed, 3),
+         'of', bytesToSize(usage.heapTotal, 3), 'total }');
+
+}
