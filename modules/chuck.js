0fd35f332a45d52564a3f8620c4b52a9826d4ec5
Toomas Vahtra <toomas.vahtra@gmail.com>
2017-09-06 19:49:09 +0300

    chuck norris jokes

---
+++ modules/chuck.js
@@ -0,0 +1,21 @@
+var trim = require('trim');
+
+module.exports = 
+{
+	/*
+	 * Get Chuck Norris joke 
+	 **/
+	get_head: function(from, text, cb) {
+              var request = require('request');
+              request('http://api.icndb.com/jokes/random', {method: 'GET'}, function (err, res, body) {
+	              if (err) {
+		              console.log(err);
+		              cb(false);
+	              } else {
+				var util = require('util');
+		              	console.log(util.inspect(body));
+				cb(trim(body.value.joke));
+	              }
+              });
+  }
+}
