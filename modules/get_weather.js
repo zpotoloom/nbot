8b381c7d406efad7104be5ea4dc7efdbf4c9a2b2
Toomas Vahtra <toomas.vahtra@gmail.com>
2018-01-04 20:52:52 +0200

    added tests for get_title and get_weather modules

--- modules/get_weather.js
+++ modules/get_weather.js
@@ -7,20 +7,19 @@ module.exports = {
 	 * Get weather for Tallinn or specified town
 	 **/
 	get_weather: function(from, text, cb) {
-	        var city = text.replace(/[^ ]* /, '');
+    var city = text.replace(/[^ ]* /, '');
 		if ( city == text ) {
-	                city = 'Tallinn';
-	        } else if ( city.length == 0 ) { city = 'Tallinn'; }
-	        weather.find({search: city, degreeType: 'C'}, function(err, result) {
-	        if(err) {
-	                console.log(err);
-	        } else {
-	                var ms = result[0].current.windspeed.split(" ")[0] / 3.6;
-	                var direction = result[0].current.winddisplay.split(" ")[2];
-			var trim = require('trim');
-	                cb(trim(result[0].current.observationpoint + ": " + result[0].current.temperature + "(" + result[0].current.feelslike + ") " + ms.toFixed(1)  + " m/s (" + direction + ") " + result[0].current.skytext));
-	        }
-	        });
-	}
+	    city = 'Tallinn';
+	  } else if ( city.length == 0 ) { city = 'Tallinn'; }
+	    weather.find({ search: city, degreeType: 'C' }, function(err, result) {
+	      if(err) {
+          cb(false);
+	      } else {
+	        var ms = result[0].current.windspeed.split(" ")[0] / 3.6;
+	        var direction = result[0].current.winddisplay.split(" ")[2];
+			    var trim = require('trim');
+	        cb(trim(result[0].current.observationpoint + ": " + result[0].current.temperature + "(" + result[0].current.feelslike + ") " + ms.toFixed(1)  + " m/s (" + direction + ") " + result[0].current.skytext));
+	      }
+	    });
+	  }
 }
-
---
+++ test/modules.js
@@ -0,0 +1,53 @@
+var assert = require('assert-callback');
+
+// URL to Title module
+var mod_get_title = require('../modules/get_title').get_title;
+
+describe('Module: get_title', function() {
+
+  it('Return title of URL google.com', function(done) {
+    mod_get_title('user', 'https://www.google.com', function(result) {
+      assert.equal(result, '[ Google ]');
+      done();
+    });
+  });
+  it('Invalid URL', function(done) {
+    mod_get_title('user', 'http://url', function(result) {
+      assert.equal(result, false);
+      done();
+    });
+  });
+  it('Invalid content-type or missing <title> tag', function(done) {
+    mod_get_title('user', 'http://ifconfig.ca', function(result) {
+      assert.equal(result, false);
+      done();
+    });
+  });
+
+});
+
+// Weather module
+var mod_get_weather = require('../modules/get_weather').get_weather;
+
+describe('Module: get_weather', function() {
+
+  it('Default location', function(done) {
+    mod_get_weather('user', '!ilm', function(result) {
+      assert.equal(result.includes('Tallinn'), true);
+      done();
+    });
+  });
+  it('Missing location', function(done) {
+    mod_get_weather('user', '!ilm ', function(result) {
+      assert.equal(result.includes('Tallinn'), true);
+      done();
+    });
+  });
+  it('Unknown location', function(done) {
+    mod_get_weather('user', '!ilm XXX', function(result) {
+      assert.equal(result, false);
+      done();
+    });
+  });
+
+});
