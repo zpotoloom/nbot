cd977b44b2241f0c766ede0a9730b4abcf6afcf2
Toomas Vahtra <toomas.vahtra@mnt.ee>
2018-05-10 22:46:45 +0300

    insult

---
+++ modules/insult.js
@@ -0,0 +1,21 @@
+var trim = require('trim');
+
+module.exports = 
+{
+	/*
+	 * Insult 
+	 **/
+	insult: function(from, text, cb) {
+              var request = require('request');
+              request('http://quandyfactory.com/insult/json', {method: 'GET'}, function (err, res, body) {
+	              if (err) {
+		              console.log(err);
+		              cb(false);
+	              } else {
+				if ( JSON.parse(body).value !== undefined ) {
+					cb(trim(JSON.parse(body).value.insult));
+				}
+	              }
+              });
+  }
+}
