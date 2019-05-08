c44156f31b216382cfb69c5782175e684c0f78ef
Toomas Vahtra <toomas.vahtra@gmail.com>
2019-05-08 17:01:15 +0300

    update weather-js, clear cache for weather-js for each run

--- modules/get_weather.js
+++ modules/get_weather.js
@@ -1,25 +1,28 @@
 // Weather plugin using bing
+// module caches something in included modules, breaks when multiple failed requests sent
+delete require.cache[require.resolve('weather-js')];
 var weather = require('weather-js');
 
 module.exports = {
 
-	/*
-	 * Get weather for Tallinn or specified town
-	 **/
-	get_weather: function(from, text, cb) {
-    var city = text.replace(/[^ ]* /, '');
-		if ( city == text ) {
-	    city = 'Tallinn';
-	  } else if ( city.length == 0 ) { city = 'Tallinn'; }
-	    weather.find({ search: city, degreeType: 'C' }, function(err, result) {
-	      if(err) {
-          cb(false);
-	      } else {
-	        var ms = result[0].current.windspeed.split(" ")[0] / 3.6;
-	        var direction = result[0].current.winddisplay.split(" ")[2];
-			    var trim = require('trim');
-	        cb(trim(result[0].current.observationpoint + ": " + result[0].current.temperature + "(" + result[0].current.feelslike + ") " + result[0].current.humidity + "% " + ms.toFixed(1)  + " m/s (" + direction + ") " + result[0].current.skytext));
-	      }
-	    });
-	  }
+    /*
+     * Get weather for Tallinn or specified town
+     **/
+    get_weather: function(from, text, cb) {
+        var city = text.replace(/[^ ]* /, '');
+        if ( city == text ) {
+            city = 'Tallinn';
+        } else if ( city.length == 0 ) { city = 'Tallinn'; }
+            weather.find({ search: city, degreeType: 'C' }, function(err, result) {
+                if(err) {
+                    cb("Try harder!");
+                } else {
+                    console.log(err);
+                    var ms = result[0].current.windspeed.split(" ")[0] / 3.6;
+                    var direction = result[0].current.winddisplay.split(" ")[2];
+                    var trim = require('trim');
+                    cb(trim(result[0].current.observationpoint + ": " + result[0].current.temperature + "(" + result[0].current.feelslike + ") " + result[0].current.humidity + "% " + ms.toFixed(1)    + " m/s (" + direction + ") " + result[0].current.skytext));
+                }
+            });
+        }
 }
