2ab5bd57d81bb4b6d920fc9da98a4f6f68e5d837
Kristjan Toots <kristjan.toots@gmail.com>
2020-02-18 14:55:25 +0200

    Quick alternative to the image search

--- modules/get_pic.js
+++ modules/get_pic.js
@@ -1,27 +1,57 @@
-var gis = require('g-i-s');
+var request = require('request');
+var fs = require('fs');
 
 module.exports = {
 
-	get_pic: function(from, text, cb) {
-		var query = text.replace(/[^ ]* /, '');
-		var q_w_opts = {
-			searchTerm: query + ' site:reddit.com',
-			queryStringAddition: "&safe=off&tbs=img:1,isz:lt,islt:vga"
+	get_pic: function (from, text, cb) {
+		var urls = [];
+		// read suitable urls from the file
+		try {
+			// Will throw exception when such file does not exist
+			urls = fs.readFileSync('./pic_reddit_urls.txt', 'utf8').split(/\r?\n/);
+
+			// Empty file has single element with empty contents
+			var hasUrls = !(urls.length === 1 && urls[0] === "");
+			if (!hasUrls) {
+				cb("Found file pic_reddit_urls.txt but it appears to be empty");
+			}
+		} catch (error) {
+			cb("Reading the urls from the file 'pic_reddit_urls.txt' has encountered an error '" + error.message + "'");
+		}
+
+		// make request to the given random url
+		var options = {
+			url: urls[getIndex(urls.length)],
+			headers: {
+				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+			}
 		};
-		gis(q_w_opts, logResults);
-		
-		function logResults(error, results) {
-		  if (error) {
-		    console.log(error);
-		  }
-		  else {
-		    var result = results[Math.floor( Math.random() * ( results.length - 1 ) )];
-		    if ( result ) {
-		    	cb(result.url);
-		    } else {
-			cb('Try harder!');
-		    }
-		  }
+
+		// choose one random response if successful
+		request(options, requestCallback)
+
+		function requestCallback(error, response, body) {
+			if (error) {
+				cb(error);
+			}
+			else {
+				try {
+					var response = JSON.parse(body);
+					var children = response.data.children;
+
+					var urls = children.map(x => x.data.url);
+					var url = urls[getIndex(urls.length)];
+
+					cb(url);
+				}
+				catch (error) {
+					cb(error.message)
+				}
+			}
+		}
+
+		function getIndex(maxLength) {
+			return Math.floor(Math.random() * Math.floor(maxLength - 1));
 		}
 	}
 }
