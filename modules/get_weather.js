050e2361d8764fcb013460aef65ff480ad0f8880
Toomas Vahtra <toomas.vahtra@gmail.com>
2020-04-30 11:50:17 +0300

    replace bing weather with wttr.in

--- modules/get_weather.js
+++ modules/get_weather.js
@@ -1,8 +1,3 @@
-// Weather plugin using bing
-// module caches something in included modules, breaks when multiple failed requests sent
-delete require.cache[require.resolve('weather-js')];
-var weather = require('weather-js');
-
 module.exports = {
 
     /*
@@ -10,25 +5,17 @@ module.exports = {
      **/
     get_weather: function(from, text, cb) {
         var city = text.replace(/[^ ]* /, '');
-        console.log(city);
-        console.log(text);
         if ( city == text ) {
             city = 'Tallinn';
         } else if ( city.length == 0 ) { city = 'Tallinn'; }
-            weather.find({ search: city, degreeType: 'C' }, function(err, result) {
-                if(err) {
-                    console.log(err);
-                    cb("Microsoft discontinued access to the MSN Weather Service API on April 15, 2016.");
-                } else {
-                    var ms = result[0].current.windspeed.split(" ")[0] / 3.6;
-                    var direction = result[0].current.winddisplay.split(" ")[2];
-                    var trim = require('trim');
-                    var addition = "";
-                    if ( result[0].current.temperature >= 25 ) {
-                        addition = "Vitun kuuma se on ! ";
-                    }
-                    cb(addition + trim(result[0].current.observationpoint + ": " + result[0].current.temperature + "(" + result[0].current.feelslike + ") " + result[0].current.humidity + "% " + ms.toFixed(1)    + " m/s (" + direction + ") " + result[0].current.skytext));
-                }
-            });
+              var request = require('request');
+              request('http://wttr.in/'+city+'?format="%l:+%c+%t+%w+%m"', {method: 'GET'}, function (err, res, body) {
+                      if (err) {
+                              console.log(err);
+                              cb(false);
+                      } else {
+                                cb(body.replace(/"/g, ''));
+                      }
+              });
         }
 }
