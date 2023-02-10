d658019840a309a17058e15365bbfe7d34f0ae21
Toomas Vahtra <toomas.vahtra@gmail.com>
2023-02-10 19:45:48 +0200

    replace broken Chuck facts api with fr version

--- modules/chuck.js
+++ modules/chuck.js
@@ -7,16 +7,30 @@ module.exports =
    **/
   chuck: function (from, text, cb) {
     var request = require('request');
-    request('http://api.icndb.com/jokes/random', { method: 'GET' }, function (err, res, body) {
-      if (err) {
-        console.log(err);
-        cb(false);
-      } else {
-        if (body !== undefined && body.length > 0 &&JSON.parse(body).value !== undefined) {
-          cb(trim(JSON.parse(body).value.joke));
+    request('http://www.chucknorrisfacts.fr/facts/random', function (error, response, body)
+    {
+      if (!error && response.statusCode == 200)
+      {
+        // extract first html p element from body with class card-text safely
+        var fact = body.match(/<p class="card-text">(.*)<\/p>/);
+        if (fact) {
+          fact = fact[1];
+          // Replace French Characters
+          fact = fact.replace(/&eacute;/g, 'é');
+          fact = fact.replace(/&egrave;/g, 'è');
+          fact = fact.replace(/&ecirc;/g, 'ê');
+          fact = fact.replace(/&ccedil;/g, 'ç');
+          fact = fact.replace(/&agrave;/g, 'à');
+          fact = fact.replace(/&acirc;/g, 'â');
+          fact = fact.replace(/&ocirc;/g, 'ô');
+          fact = fact.replace(/&icirc;/g, 'î');
+          fact = fact.replace(/&ucirc;/g, 'û');
+          // replace html &#039; with '  (single quote)
+          fact = fact.replace(/&#039;/g, "'");
+          cb(trim(fact));
         } else {
-	  cb("500");
-	}
+          cb('Chuck Norris is not amused.');
+        }
       }
     });
   }
